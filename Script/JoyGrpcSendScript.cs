faafc4072343782c5e47c4f92d399969fd39acff
Kim Seong Hyeon <dhkdwk0215@naver.com>
2022-08-24 23:21:46 +0900

    Add files via upload

--- Script/JoyGrpcSendScript.cs
+++ Script/JoyGrpcSendScript.cs
@@ -10,31 +10,36 @@ using System.Threading.Tasks;
 using RobotPose;
 using Valve.VR;
 
+
+[RequireComponent(typeof(SteamVR_Behaviour_Pose))]
 public class JoyGrpcSendScript : MonoBehaviour
 {
     public Channel channel;
+    public SteamVR_Behaviour_Pose VRpose;
 
     void Start()
     {
         channel =  new Channel("192.168.0.4:50051", ChannelCredentials.Insecure);
-        call = PoseClient.GetPose();
+        // call = PoseClient.GetPose();
         
     }
 
     void Update()
     {
-        UpdateTransform();
+        
+        VRpose = gameObject.GetComponent<SteamVR_Behaviour_Pose>();
+        VRpose.UpdateTransform();
     
     }
 
     private async Task SendPosition()
     {
-        Vector3 tmp = transform.position;
+        Vector3 tmp = VRpose.transform.position;
         var px = tmp.x;
         var py = tmp.y;
         var pz = tmp.z;
         var req = new GetPose{Id = "r", x=px, y=  py, z= pz};
-        await call.GetPose.WriteAsync(req);
+        // await call.GetPose.WriteAsync(req);
 
     }
 
